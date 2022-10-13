867c80bfb5e94da033e485289182778e977d2d33
nikfp <nikprotsman@gmail.com>
2022-10-13 17:42:01 -0600

    redesigned how the game is structured for simpler files

---
+++ src/lib/stores/allowers.ts
@@ -0,0 +1,14 @@
+import { derived } from "svelte/store";
+import { _game } from "./_game";
+
+export const allowers = derived(_game, ($_game) => {
+  let size = $_game.size;
+  let count = $_game.mineCount;
+  console.log('size is: ' + size);
+  return {
+    canIncreaseSize: $_game.size < 15,
+    canDecreaseSize: $_game.size > 5,
+    canIncreaseMines: count < (size * size / 2),
+    canDecreaseMines: count > ((size * size) / 4)
+  };
+});
--- src/lib/stores/game.ts
+++ src/lib/stores/game.ts
@@ -1,88 +1,32 @@
-import { writable, get, derived } from "svelte/store";
-
-export interface ISquare {
-  covered: boolean;
-  row: number;
-  col: number;
-  mine: boolean;
-}
-
-interface IGame {
-  board: ISquare[][];
-  mineCount: number;
-  size: number;
-}
-
-const _game = writable<IGame>({
-  size: 0,
-  mineCount: 0,
-  board: [],
-});
-
-function buildBoard(size: number, mineCount: number) {
-  const squareCount = size * size;
-  const random = Array(squareCount).fill(false);
-  let placed = 0;
-
-  while (placed <= mineCount) {
-    const location = Math.floor(Math.random() * squareCount);
-
-    if (random[location] === true) {
-      continue;
-    }
-    random[location] = true;
-    placed++;
-  }
-
-  const squares: ISquare[] = random.map((el) => {
-    return {
-      mine: el,
-      row: 0,
-      col: 0,
-      covered: true,
-    };
-  });
-
-  const board: ISquare[][] = [];
-
-  while (squares.length > 0) {
-    const row = squares.splice(0, mineCount);
-    board.push(row);
-  }
-
-  return board;
-}
-
-export const allowers = derived(_game, ($_game) => {
-  return {
-    canIncreaseSize: $_game.size < 15,
-    canDecreaseSize: $_game.size > 5,
-  };
-});
+import { get, derived } from "svelte/store";
+import { _game } from "./_game";
+import { buildBoard } from "./_buildBoard";
+import { allowers } from "./allowers";
 
 export const game = {
   subscribe: _game.subscribe,
-  set: _game.set,
-  update: _game.update,
   initialize: () => {
     _game.update((value) => {
       return {
         ...value,
         size: 10,
-        mineCount: 10,
-        board: buildBoard(10, 10),
+        mineCount: 25,
+        board: buildBoard(10, 25),
       };
     });
   },
   increaseSize: () => {
     _game.update((value) => {
-      const getAllowers = get(allowers);
-      const canIncrease = getAllowers.canIncreaseSize;
+      const canIncrease = get(allowers).canIncreaseSize;
       if (!canIncrease) return value;
 
+      const size = value.size + 1;
+      const minMines = Math.floor(size * size / 4);
+      const mineCount = value.mineCount > minMines ? value.mineCount : minMines;
       return {
         ...value,
-        size: value.size + 1,
+        size, 
+        mineCount,
         board: buildBoard(value.size + 1, value.mineCount),
       };
     });
@@ -92,15 +36,43 @@ export const game = {
       const getAllowers = get(allowers);
       const canDecrease = getAllowers.canDecreaseSize;
       if (!canDecrease) return value;
+      const size = value.size - 1;
 
-      const maxCount = Math.floor((value.size * value.size) / 2);
+      const maxCount = Math.floor((size * size) / 2);
       const mineCount = value.mineCount > maxCount ? maxCount : value.mineCount;
       return {
         ...value,
-        size: value.size - 1,
+        size,
         mineCount,
         board: buildBoard(value.size - 1, mineCount),
       };
     });
   },
+  increaseMines: () => {
+    _game.update((value) => {
+      const canIncrease = get(allowers).canIncreaseMines;
+      if (!canIncrease) return value;
+
+      const mineCount = value.mineCount + 1;
+      return {
+        ...value,
+        mineCount,
+        board: buildBoard(value.size, mineCount),
+      };
+    });
+  },
+  decreaseMines: () => {
+    _game.update(value => {
+      const canDecrease = get(allowers).canDecreaseMines;
+      if(!canDecrease) return value;
+
+      const mineCount = value.mineCount - 1;
+
+      return {
+        ...value,
+        mineCount,
+        board: buildBoard(value.size, mineCount)
+      }
+    })
+  }
 };
